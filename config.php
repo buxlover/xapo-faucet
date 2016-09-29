a3a4de3c816670ffa99124dad28ca7ffbaf50783
buxlover <buxlover100@gmail.com>
2016-09-29 13:08:22 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$mysqlHost     = 'localhost';
+$mysqlUsername = 'root';
+$mysqlPassword = '7382844';
+$mysqlDatabase = 'faucet';
+
+$dbdsn = "mysql:host=$mysqlHost;dbname=$mysqlDatabase";
+
+$myHashKey = "KtBI0gVkpEoI4U6NVoQbfeDE5D01w2Wv";
